7241b54f8589f3c770f69c039a10047a4f798d25
Keith Butler <keithbutler2001@gmail.com>
2023-11-15 10:20:40 +0000

    reviews lambda

---
+++ lambdas/getAllReviews.ts
@@ -0,0 +1,83 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    // Print Event
+    console.log("Event: ", event);
+    // console.log("Event: ", JSON.stringify(event?.queryStringParameters));
+    // const parameters = event?.queryStringParameters;
+    // const movieId = parameters ? parseInt(parameters.movieId) : undefined;
+    const parameters  = event?.pathParameters;
+    const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+    
+    if (!movieId) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Missing movie Id" }),
+      };
+    }
+
+    // Print Event
+    console.log("Event: ", event);
+
+    const commandOutput = await ddbClient.send(
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+          // Key: { movieId: movieId },
+          // TODO add listing all movie reviews
+      })
+    );
+    if (!commandOutput.Items) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
+      };
+    }
+    const body = {
+      data: commandOutput.Items,
+    };
+
+    // Return Response
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
