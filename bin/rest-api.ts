f90c9cf52b1de26f07c51030b6f6be5e75e3e8d1
Keith Butler <keithbutler2001@gmail.com>
2023-11-26 14:49:38 +0000

    added authentatication stack

--- bin/rest-api.ts
+++ bin/rest-api.ts
@@ -2,6 +2,9 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { RestAPIStack } from "../lib/rest-api-stack";
+import { AuthAppStack } from '../lib/auth-app-stack';
+
 
 const app = new cdk.App();
+new AuthAppStack(app, "AwsServerlessAuthStack", { env: { region: "eu-west-1" } });
 new RestAPIStack(app, "RestAPIStack", { env: { region: "eu-west-1" } });
